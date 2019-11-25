f76b1f13565b280994cc6b8324ae584c9ee558a7
Catherine Barnett <ca.barnett06@gmail.com>
2019-11-25 13:41:09 -0500

    ajax for api url

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 var current = new Date();
 var date = (current.getMonth()+1) + '/' + current.getDate() + '/' + current.getFullYear();
 
+$('#cityName').submit(function(e){
+    e.preventDefault();    
+    var userInput = $('#userInput').val().trim();
+    var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput},us&units=imperial&APPID=3b5715c4cc3d80c499e1192a073d5982`;    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
